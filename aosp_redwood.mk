e9a9a3fc9a2d9c225feec55725647a2db9efce1f
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2024-05-01 00:53:18 +0600

    redwood: PixelOS
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- aosp_redwood.mk
+++ aosp_redwood.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2025 PixelOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,14 +12,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from redwood device
 $(call inherit-product, device/xiaomi/redwood/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_BRAND := POCO
 PRODUCT_DEVICE := redwood
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 22101320G
-PRODUCT_NAME := lineage_redwood
+PRODUCT_NAME := aosp_redwood
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
