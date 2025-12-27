bf9647891cd4cd56182f5621f50d2578f53ac1ad
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-12-28 04:35:35 +0600

    redwood: Remove overlay for Aperture
    We don't want or use aperture.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- device.mk
+++ device.mk
@@ -30,9 +30,6 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
-    ApertureOverlayRedwood
-
-PRODUCT_PACKAGES += \
     FrameworkOverlayRedwood \
     SettingsOverlayRedwood \
     SettingsProviderOverlayRedwoodCN \
