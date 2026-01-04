4571e46db6964663d1169be19bc2b5b61d25927a
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-01-04 16:14:29 +0600

    redwood: Update blobs to OS2.0.16.0.UMSMIXM
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- lineage_redwood.mk
+++ lineage_redwood.mk
@@ -18,8 +18,8 @@ PRODUCT_NAME := lineage_redwood
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="redwood_global-user 14 UKQ1.240624.001 OS2.0.13.0.UMSMIXM release-keys" \
-    BuildFingerprint=POCO/redwood_global/redwood:14/UKQ1.240624.001/OS2.0.13.0.UMSMIXM:user/release-keys \
+    BuildDesc="redwood_global-user 14 UKQ1.240624.001 OS2.0.16.0.UMSMIXM release-keys" \
+    BuildFingerprint=POCO/redwood_global/redwood:14/UKQ1.240624.001/OS2.0.16.0.UMSMIXM:user/release-keys \
     DeviceProduct=redwood \
     SystemName=redwood_global
 # Exclude apps
